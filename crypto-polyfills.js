490ba33d9ea19f169e4f4e0f30aa1ec204a544f1
Erlend Oftedal <erlend@oftedal.no>
2016-01-19 11:54:43 +0100

    Update crypto-polyfills.js

--- crypto-polyfills.js
+++ crypto-polyfills.js
@@ -1,17 +1,17 @@
 //These functions all characters are ascii / Latin-1
 
 Number.prototype.toHex = function() { return ("0" + this.toString(16)).substr(-2) }
+Number.prototype.toChar = function() { return String.fromCharCode(this) }
 
 String.prototype.base64Decode = function() { return atob(this) }
 String.prototype.base64Encode = function() { return btoa(this) }
 
-String.prototype.toByteArray = function() { return this.match(/./g).map(function(c){ return c.charCodeAt(0) }) }
-
-Array.prototype.hexEncode = function() { return this.map(function(x) { return x.toHex() }).join("") }
-
-String.prototype.toByteArray = function() { return this.match(/./g).map(function(c) { return c.charCodeAt(0) }) }
-
 String.prototype.hexDecode = function() { return this.match(/../g).map(function(c) { return parseInt(c, 16) }) }
 String.prototype.hexEncode = function() { return this.toByteArray().hexEncode() }
 
+String.prototype.toByte = function() { return this.charCodeAt(0) }
+String.prototype.toByteArray = function() { return this.match(/./g).map(function(c){ return c.toByte() }) }
 Array.prototype.asString = function() { return String.fromCharCode.apply(null, this) }
+
+Array.prototype.hexEncode = function() { return this.map(function(x) { return x.toHex() }).join("") }
+
