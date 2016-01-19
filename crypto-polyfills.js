d549951f69640da292c418cf758c6b6f86efca95
Erlend Oftedal <erlend@oftedal.no>
2016-01-19 11:37:28 +0100

    Create crypto-polyfills.js

---
+++ crypto-polyfills.js
@@ -0,0 +1,15 @@
+Number.prototype.toHex = function() { return ("0" + this.toString(16)).substr(-2) }
+
+String.prototype.base64Decode = function() { return atob(this) }
+String.prototype.base64Encode = function() { return btoa(this) }
+
+String.prototype.toByteArray = function() { return this.match(/./g).map(function(c){ return c.charCodeAt(0) }) }
+
+Array.prototype.hexEncode = function() { return this.map(function(x) { return x.toHex() }).join("") }
+
+String.prototype.toByteArray = function() { return this.match(/./g).map(function(c) { return c.charCodeAt(0) }) }
+
+String.prototype.hexDecode = function() { return this.match(/../g).map(function(c) { return parseInt(c, 16) }) }
+String.prototype.hexEncode = function() { return this.toByteArray().hexEncode() }
+
+Array.prototype.asString = function() { return String.fromCharCode.apply(null, this) }
