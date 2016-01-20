901d34c8315e57c4f8f74ae7cad8ee38b47bd837
Erlend Oftedal <erlend@oftedal.no>
2016-01-20 12:56:09 +0100

    Update crypto-polyfills.js

--- crypto-polyfills.js
+++ crypto-polyfills.js
@@ -11,7 +11,9 @@ String.prototype.hexEncode = function() { return this.toByteArray().hexEncode() 
 
 String.prototype.toByte = function() { return this.charCodeAt(0) }
 String.prototype.toByteArray = function() { return this.match(/./g).map(function(c){ return c.toByte() }) }
-Array.prototype.asString = function() { return String.fromCharCode.apply(null, this) }
 
+Array.prototype.asString = function() { return String.fromCharCode.apply(null, this) }
 Array.prototype.hexEncode = function() { return this.map(function(x) { return x.toHex() }).join("") }
+Array.prototype.xor = function(ar) { var me = this; return (me.length > ar.length ? me : ar).map(function(x,i) { return me[i] ^ ar[i] }) }
+
 
